afb6e5ce086e2eef4883afc7804eca3b53f5964b
booyouon <66927111+booyouon@users.noreply.github.com>
2021-07-09 12:59:08 -0700

    Dropdown added
    - dropdown added for the navbar
    - UO banner css minor change where the cursor is now a pointer only while on the image instead of the entire div.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const women = document.querySelector("#women");
+const womenMenu = document.querySelector('#womenMenu');
+const men = document.querySelector("#men");
+const menMenu = document.querySelector("#menMenu");
+const beauty = document.querySelector("#beauty");
+const beautyMenu = document.querySelector("#beautyMenu");
+const sale = document.querySelector("#sale");
+const saleMenu = document.querySelector("#saleMenu");
 
+hover(beauty,beautyMenu);
+hover(men,menMenu);
+hover(women,womenMenu);
+hover(sale,saleMenu);
+
+function hover(location, menu) {
+    location.addEventListener('mouseover', () => {
+        menu.setAttribute("style", "display:flex");
+    });
+    location.addEventListener('mouseout', () => {
+        menu.setAttribute("style", "display:none");
+    });
+}
