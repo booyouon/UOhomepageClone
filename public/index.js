9ea697ec8ddf4f7584bc5fdac11b3a2e652490de
booyouon <66927111+booyouon@users.noreply.github.com>
2021-07-16 02:07:04 -0700

    start script, sort, sale
    - npm start in terminal will now run nodemon uo.js
    - sort feature works now in the listing pages
    - sale portion now functional and displays on the product listings

--- public/index.js
+++ public/index.js
@@ -76,6 +76,7 @@ const menListingRow = document.querySelector("#menListingRow");
 const womenListingRow = document.querySelector("#womenListingRow");
 const beautyListingRow = document.querySelector("#beautyListingRow");
 const saleListingRow = document.querySelector("#saleListingRow");
+const sort = document.querySelector("#sort");
 
 // container for sales listing
 let menListing = [
@@ -84,8 +85,8 @@ let menListing = [
         name: "Man in Black",
         img1: "https://images.unsplash.com/photo-1617113930975-f9c7243ae527?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
         img2: "https://images.unsplash.com/photo-1617127365659-c47fa864d8bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-        price: "$49.99",
-        sale: "$39.99",
+        price: "49.99",
+        sale: "39.99",
         href: "#"
     },
     {
@@ -93,7 +94,7 @@ let menListing = [
         name: "Man in Blue",
         img1: "https://images.unsplash.com/photo-1617137968427-85924c800a22?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
         img2: "https://images.unsplash.com/photo-1617137984095-74e4e5e3613f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-        price: "$59.99",
+        price: "59.99",
         href: "#"
     },
     {
@@ -101,7 +102,7 @@ let menListing = [
         name: "Tan Coat",
         img1: "https://images.unsplash.com/photo-1552374196-1ab2a1c593e8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
         img2: "https://images.unsplash.com/photo-1553143820-6bb68bc34679?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=623&q=80",
-        price: "$39.99",
+        price: "39.99",
         href: "#"
     },
 ];
@@ -112,8 +113,8 @@ let womenListing = [
         name: "Black Striped Pants",
         img1: "https://images.unsplash.com/photo-1551048632-24e444b48a3e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
         img2: "https://images.unsplash.com/photo-1551048633-8c03f448307f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
-        price: "$49.99",
-        sale: "$39.99",
+        price: "49.99",
+        sale: "39.99",
         href: "#"
     },
     {
@@ -121,7 +122,7 @@ let womenListing = [
         name: "Cali Motor Tee",
         img1: "https://images.unsplash.com/photo-1621694691103-e326ece9252b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
         img2: "https://images.unsplash.com/photo-1621694691240-b63c0deaba70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
-        price: "$29.99",
+        price: "29.99",
         href: "#"
     },
     {
@@ -129,7 +130,7 @@ let womenListing = [
         name: "Black Ring Top",
         img1: "https://images.unsplash.com/photo-1621784562807-cb450c2f5efc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
         img2: "https://images.unsplash.com/photo-1621784563286-84f7646ef221?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
-        price: "$39.99",
+        price: "39.99",
         href: "#"
     },
 ];
@@ -140,8 +141,8 @@ let beautyListing = [
         name: "Soap",
         img1: "https://images.unsplash.com/photo-1589060040782-234fa4ee0b61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
         img2: "https://images.unsplash.com/photo-1589060040843-7a31813e6fb0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-        price: "$9.99",
-        sale: "$6.99",
+        price: "9.99",
+        sale: "6.99",
         href: "#"
     },
     {
@@ -149,7 +150,7 @@ let beautyListing = [
         name: "Make up set",
         img1: "https://images.unsplash.com/photo-1522338242992-e1a54906a8da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
         img2: "https://images.unsplash.com/photo-1589825853236-7a9e04b56687?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-        price: "$39.99",
+        price: "39.99",
         href: "#"
     },
     {
@@ -157,7 +158,7 @@ let beautyListing = [
         name: "Tote Bag",
         img1: "https://images.unsplash.com/photo-1572196284554-4e321b0e7e0b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
         img2: "https://images.unsplash.com/photo-1544816155-12df9643f363?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-        price: "$19.99",
+        price: "19.99",
         href: "#"
     },
 ];
@@ -168,8 +169,8 @@ let saleListing = [
         name: "Man in Black",
         img1: "https://images.unsplash.com/photo-1617113930975-f9c7243ae527?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
         img2: "https://images.unsplash.com/photo-1617127365659-c47fa864d8bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-        price: "$49.99",
-        sale: "$39.99"
+        price: "49.99",
+        sale: "39.99",
         href: "#"
     },
     {
@@ -177,8 +178,8 @@ let saleListing = [
         name: "Black Striped Pants",
         img1: "https://images.unsplash.com/photo-1551048632-24e444b48a3e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
         img2: "https://images.unsplash.com/photo-1551048633-8c03f448307f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
-        price: "$49.99",
-        sale: "$39.99",
+        price: "49.99",
+        sale: "39.99",
         href: "#"
     },
     {
@@ -186,35 +187,32 @@ let saleListing = [
         name: "Soap",
         img1: "https://images.unsplash.com/photo-1589060040782-234fa4ee0b61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
         img2: "https://images.unsplash.com/photo-1589060040843-7a31813e6fb0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-        price: "$9.99",
-        sale: "$6.99",
+        price: "9.99",
+        sale: "6.99",
         href: "#"
     },
 ];
 
 //generates the listing for men.html
-if (menListingRow) {
-    makeListing(menListing, menListingRow);
-    hoverImg(menListing);
-};
+generateListing(menListing, menListingRow);
 
 //generates the listing for women.html
-if (womenListingRow) {
-    makeListing(womenListing, womenListingRow);
-    hoverImg(womenListing);
-};
+generateListing(womenListing, womenListingRow);
 
 //generates the listing for beauty.html
-if (beautyListingRow) {
-    makeListing(beautyListing, beautyListingRow);
-    hoverImg(beautyListing);
-};
+generateListing(beautyListing, beautyListingRow);
 
 //generates the listing for sale.html
-if (saleListingRow) {
-    makeListing(saleListing, saleListingRow);
-    hoverImg(saleListing);
-};
+generateListing(saleListing, saleListingRow);
+
+//function to generate each listing page
+function generateListing(listing, location) {
+    if (location) {
+        makeListing(listing, location);
+        hoverImg(listing);
+        sortListing(listing, location);
+    }
+}
 
 // appends the listings onto the designated page
 function makeListing(listing, location) {
@@ -234,7 +232,16 @@ function makeListing(listing, location) {
         imgListing.src = content.img1;
         itemName.textContent = content.name;
         itemName.href = content.href;
-        itemPrice.textContent = content.price;
+        itemPrice.textContent = `$${content.price}`;
+
+        //checks if the listing has a sale property and if so then updates the page to have the sale price
+        if(content.sale) {
+            let itemSale = document.createElement("p");
+            newListing.append(itemSale);
+            itemSale.textContent = `$${content.sale}`;
+            itemPrice.setAttribute("style", "text-decoration: line-through;");
+            itemSale.setAttribute("style", "color: red;");
+        }
     
         location.append(newListing);
     };
@@ -244,7 +251,7 @@ function makeListing(listing, location) {
 function hoverImg(listing) {
     let x = [];
     for (let i = 0; i < listing.length; i++) {
-        x.push(document.getElementById(i));
+        x.push(document.getElementById(listing[i].id));
         x[i].addEventListener('mouseover', () => {
             x[i].src = listing[i].img2;
         });
@@ -253,6 +260,30 @@ function hoverImg(listing) {
         });
     };
 };
+
+//sorts the page listing by the selected sort option
+function sortListing(listing, location) {
+    sort.addEventListener('change', function() {
+        console.log('You selected: ', this.value);
+        if(sort.value === "low") {
+            listing = listing.sort((a, b) => {
+                return a.price - b.price;
+            });
+        } else if (sort.value === "high") {
+            listing = listing.sort((a, b) => {
+                return b.price - a.price;
+            });
+        } else {
+            listing = listing.sort((a, b) => {
+                return a.id - b.id;
+            });
+        }
+        location.innerHTML = "";
+        makeListing(listing, location);
+        hoverImg(listing);
+      });
+}
+
 
 /*
 shipping.html, cart.html js
