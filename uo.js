edd6a76fa2e2b8451d02b61b58b7dbbefcdb0968
Vince Abuyuan <vinceabuyuan@Vinces-MacBook-Air.local>
2021-09-28 11:32:36 -0700

    changed mongodb local to mongodb cluster

--- uo.js
+++ uo.js
@@ -17,10 +17,14 @@ const methodOverride = require("method-override");
 const exp = require("constants");
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/uoCart", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    process.env.MONGODB_URI ||
+      "mongodb+srv://sample:ihKvH2G1QED6in84@cluster0.yo7eo.mongodb.net/uoclone?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("mongo connection open");
   })
